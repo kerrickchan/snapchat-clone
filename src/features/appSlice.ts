92ab62e36ff0744406ae374c857a66497505f662
Eric Chan <eric88ordinary@gmail.com>
2021-03-05 00:18:12 +0800

    starting the build

---
+++ src/features/appSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppThunk, RootState } from '../app/store';
+
+interface AppState {
+  value: number;
+}
+
+const initialState: AppState = {
+  value: 0,
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { incrementByAmount } = appSlice.actions;
+
+export const selectApp = (state: RootState) => state.app.value;
+
+export default appSlice.reducer;
